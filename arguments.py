856542607090f4a9053cb93d07cbb3126a591c4e
Abdouljr <maigaabdouljr@gmail.com>
2024-02-21 00:06:02 +0000

    arguments en ligne de commandes

---
+++ arguments.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+class Arguments:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Password Cracker")
+
+    def args(self):
+        self.parser.add_argument("-f", "--file", dest="file", help="Chemin du dictionnaire de mot de passes",
+                                 required=False)
+        self.parser.add_argument("-g", "--gen", dest="gen", help="Générer un hash MD5 pour le mot passe",
+                                 required=False)
+        self.parser.add_argument("-m", "--md5", dest="md5", help="Mot de passe hacher (MD5)",
+                                 required=False)
+        return self.parser.parse_args()
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import random
 import string
 import hashlib
+import sys
+from arguments import Arguments
+
+
+argument = Arguments()
+args = argument.args()
 
 
 class Cracker:
     def __init__(self):
-        self.mot_de_passe = input("Quel est le mot de passe : ")
+        self.mot_de_passe = ""
         self.lettres = string.ascii_letters
         self.tantative = 0
         self.suiv = ''
         self.resultat = ''
+        self.trouve = False
+        self.args = args
 
     def crackerme(self):
         for i in range(len(self.mot_de_passe)):
@@ -20,18 +28,30 @@ class Cracker:
             self.resultat += self.suiv
         return self.resultat
 
-    def hash_crack(self):
-        hashlib.md5(self.mot_de_passe.encode('utf8')).hexdigest()
+    def crack_dict(self):
+        # self.mot_de_passe = hashlib.md5(self.mot_de_passe.encode('utf8')).hexdigest()
         try:
-            mots_fr = open("/home/abdouljr/Dev/liste_mots.txt", 'r')
+            mots_fr = open(str(self.args.file), 'r')
             for mot in mots_fr.readlines():
+                self.tantative += 1
                 mot = mot.strip('\n').encode('utf8')
-                print(mot)
+                mot_md5 = hashlib.md5(mot).hexdigest()
+                print(self.tantative, "", mot)
+                if self.args.md5 == mot_md5:
+                    print("Mot de passe trouvé: mot( " + str(mot) + " ) " + self.args.md5)
+                    self.trouve = True
+                    break
+            if not self.trouve:
+                print("Mot de passe non trouvé !!")
+            self.args.file.close()
         except FileNotFoundError:
             print("Erreur: chemin du fichier incorrect ou fichier introuvable .")
+            sys.exit(1)
         except Exception as ereur:
             print("Erreur: " + str(ereur))
+            sys.exit(2)
 
 
 cracker = Cracker()
-cracker.hash_crack()
+print(args.md5)
+
