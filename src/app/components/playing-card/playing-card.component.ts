86df982d112d5bbb7705639d8412b943418de9eb
Abdouljr <maigaabdouljr@gmail.com>
2024-09-25 17:46:14 +0000

    debut du signal input

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, input, InputSignal } from '@angular/core';
 import { Monster } from '../../models/moster.model';
 
 @Component({
@@ -11,5 +11,8 @@ import { Monster } from '../../models/moster.model';
 export class PlayingCardComponent {
   // on peut passer l'attribut required true pour le rendre obligatoire @Input({required: true})
   // {transform: (value: Monster) => {}} pour faire des transformation avant l'utilisation du variable;
- @Input() moster: Monster = new Monster();
+  //  @Input() moster: Monster = new Monster();
+
+  // signal input
+ moster: InputSignal<Monster> = input(new Monster());
 }
