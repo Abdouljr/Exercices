8552ab6048f8c1adbb37a0d98ec2987bb3e177d9
Abdouljr <maigaabdouljr@gmail.com>
2024-09-25 18:00:26 +0000

    création de search-bar compenent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Monster } from './models/moster.model';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlayingCardComponent],
+  imports: [RouterOutlet, PlayingCardComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
