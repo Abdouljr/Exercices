64f894d92dc52ea3f6f339e550c423a1e399e550
Abdouljr <maigaabdouljr@gmail.com>
2024-09-25 17:53:36 +0000

    création du model moster

---
+++ src/app/models/moster.model.ts
@@ -0,0 +1,8 @@
+export class Monster {
+    name: string = "My Moster";
+    hp: number = 40;
+    figureCaption: string = "N°025 Monster";
+    attackName: string = "Geo Impact";
+    attackStrength: number = 60;
+    attackDescription: string = "This is a long description of a monster attack. Probably something to do with electricity...";
+}
